11e741462c039d5a50a4273eb4b65e239112d517
Holly Rhodes <hllyrhodes@gmail.com>
2016-07-16 19:11:48 -0400

    plane created, walls built, plane tilting

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -3,14 +3,53 @@ using System.Collections;
 
 public class PlayerController : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void FixedUpdate ()
+    public Rigidbody rb;
+ 
+    public float speed;
+    public float turnSpeed;
+
+    // Use this for initialization
+    void Start()
     {
-	
-	}
+        rb = GetComponent<Rigidbody>();
+
+
+    }
+
+    //// Update is called once per frame
+    //void FixedUpdate()
+    //{
+    //    float movehorizontal = Input.GetAxis("Horizontal");
+    //    float movevertical = Input.GetAxis("Vertical");
+    //    Vector3 force = new Vector3(movehorizontal, 0, movevertical);
+    //    rb.AddForce(force * speed);
+    //}
+
+    void FixedUpdate()
+    {
+        if (Input.GetAxis("Horizontal") > 0)
+        { //if the right arrow is pressed
+            
+            transform.Rotate(0, turnSpeed * Time.deltaTime, -10 * Time.deltaTime, Space.World); //and then turn the plane
+        }
+        if (Input.GetAxis("Horizontal") < 0)
+        { //if the left arrow is pressed
+            transform.Rotate(0, turnSpeed * Time.deltaTime, 10 * Time.deltaTime, Space.World); //The X-rotation turns the plane - the Z-rotation tilts it
+        }
+        if (Input.GetAxis("Vertical") > 0)
+        { //if the up arrow is pressed
+            transform.Rotate(10 * Time.deltaTime, 0, 0);
+        }
+        if (Input.GetAxis("Vertical") < 0)
+        { //if the down arrow is pressed
+            transform.Rotate(-10 * Time.deltaTime, 0, 0);
+        }
+
+        float movehorizontal = Input.GetAxis("Horizontal");
+        float movevertical = Input.GetAxis("Vertical");
+        Vector3 force = new Vector3(movehorizontal, 0, movevertical);
+        rb.AddForce(force * speed);
+    }
+
 }
+
