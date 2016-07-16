dab981f6b29ea25b9789a56306ce8dcf68fa119f
Holly Rhodes <hllyrhodes@gmail.com>
2016-07-16 17:43:38 -0400

    initial commit

---
+++ Assets/Scripts/groundController.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NewBehaviourScript : MonoBehaviour {
+
+    public Rigidbody rb;
+    public float speed;
+
+	// Use this for initialization
+	void Start ()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+	
+	// Update is called once per frame
+	void FixedUpdate ()
+    {
+        float movehorizontal = Input.GetAxis("Horizontal");
+        float movevertical = Input.GetAxis("Vertical");
+    }
+}
